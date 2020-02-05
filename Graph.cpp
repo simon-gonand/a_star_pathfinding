b3e411aaa57e5aa105184b41fc9ebdd0156ac325
simon-gonand <simon.gonand@etu.univ-amu.fr>
2020-02-05 15:56:22 +0000

    a_star working

--- Graph.cpp
+++ Graph.cpp
@@ -18,24 +18,26 @@ int Graph::heuristicCalculation(Node n, Node goal) {
 std::vector<Node> Graph::insertionSortPriority(std::vector<Node> vec, Node goal) {
 	if (vec.size() == 1 || vec.empty()) return vec;
 	for (unsigned i = 1; i < vec.size(); ++i) {
-		int tmp = heuristicCalculation(vec[i], goal);
+		int toCompare = heuristicCalculation(vec[i], goal);
+		Node tmp = vec[i];
 		int j = i;
-		while (j > 0 && heuristicCalculation(vec[j - 1], goal) > tmp) {
+		while (j > 0 && heuristicCalculation(vec[j - 1], goal) > toCompare) {
 			vec[j] = vec[j - 1];
 			--j;
 		}
-		vec[j] = vec[i];
+		vec[j] = tmp;
 	}
+	return vec;
 }
 
 std::vector<Node> Graph::searchPath(Node start, Node goal) {
-	std::vector<Node> path = std::vector<Node>();
+	std::map<Node, Node>* pathThrough = new std::map<Node, Node>();
 	std::map<Node, double> cost = std::map<Node, double>();
 
 	std::vector<Node> frontier;
 	frontier.push_back(start);
 
-	path.push_back(start);
+	pathThrough->insert(std::pair<Node, Node>(start, start));
 	cost.insert(std::pair<Node, double>(start, 0));
 
 	while (!frontier.empty()) {
@@ -53,11 +55,23 @@ std::vector<Node> Graph::searchPath(Node start, Node goal) {
 				|| new_cost < cost[iter->first]) {
 				cost.insert(std::pair<Node, double>(iter->first, new_cost));
 				frontier.push_back(iter->first);
-				path.push_back(current);
+				if (pathThrough->find(iter->first) != pathThrough->end())
+					pathThrough->at(iter->first) = current;
+				else
+					pathThrough->insert(std::pair<Node, Node>(iter->first, current));
 			}
 			frontier = insertionSortPriority(frontier, goal);
 			++iter;
 		}
 	}
+	std::vector<Node> path;
+	Node current = goal;
+
+	while (current.getId() != start.getId()) {
+		path.push_back(current);
+		current = pathThrough->at(current);
+	}
+	path.push_back(start);
+	std::reverse(path.begin(), path.end());
 	return path;
 }
