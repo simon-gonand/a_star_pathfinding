b801e73170bc029a1b1bdf7f5aa504a8ee68f325
simon-gonand <simon.gonand@etu.univ-amu.fr>
2020-02-05 11:44:04 +0000

    edges test

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 int main() {
 	std::string str = "random64_4_1485816605.dot";
 	Parser* p = new Parser(str);
-	//Graph g = Graph();
 	Graph g = p->loadFile();
 
 	std::cout << "---------------- ALL NEIGHBOURS ----------------" << std::endl;
@@ -14,7 +13,7 @@ int main() {
 		for (std::map<Node, double>::iterator iter(g.getNodeList()->at(i).getEdges()->begin());
 			iter != g.getNodeList()->at(i).getEdges()->end();
 			++iter) {
-			std::cout << iter->first.getId();
+			std::cout << iter->first.getId() << " weight : " << iter->second << std::endl;
 		}
 		std::cout << std::endl;
 	}
